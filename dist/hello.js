398d03c623391508d36b0064f22c12ad4b0b3336
김기홍 <68697807+KimGiHong@users.noreply.github.com>
2021-03-06 19:44:07 +0900

    [Study TS Variables]

--- dist/hello.js
+++ dist/hello.js
@@ -1,5 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var util_1 = require("./util");
-var value = util_1.default(1, 2);
+import add from './util.js';
+var value = add(1, 2);
 console.log(value);
+//# sourceMappingURL=hello.js.map
